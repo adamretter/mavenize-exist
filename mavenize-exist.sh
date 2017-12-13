8627c47bd8fbc3f1aa42ddd1560078be028e0e3b
Adam Retter <adam.retter@googlemail.com>
2017-12-13 01:06:24 +0000

    fixup! Start of some scripts to Mavenize eXist

--- mavenize-exist.sh
+++ mavenize-exist.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 #set -x	# enable for debugging
 
 SRC_DIR=/Users/aretter/code/exist-for-release
 
-DEST_DIR="`pwd`/target"
+OUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEST_DIR="${OUR_DIR}/target"
 
 
 ##
-# Creates a Module
+# Creates a Maven Module
 #
 # @param $1 The module name
 ##
@@ -42,13 +43,12 @@ function copy_pom {
 # @param $1 The module name
 # @param $2 An array of package names which contain the sources
 ##
-function copy_main_java {
+function copy_main_java_pkgs {
 	local MODULE_NAME=$1
 	local MAIN_JAVA_SOURCES=("${!2}")
-	local MODULE_DIR="${DEST_DIR}/${1}"
 
 	local MAIN_JAVA_FIND_ARGS=('-name' '*.java')
-	copy_mvn "${MODULE_NAME}" "${SRC_DIR}/src" MAIN_JAVA_SOURCES[@] 'src/main/java' MAIN_JAVA_FIND_ARGS[@]
+	copy_mvn_pkgs "${MODULE_NAME}" "${SRC_DIR}/src" MAIN_JAVA_SOURCES[@] 'src/main/java' MAIN_JAVA_FIND_ARGS[@]
 }
 
 ##
@@ -57,13 +57,12 @@ function copy_main_java {
 # @param $1 The module name
 # @param $2 An array of package names which contain the resources
 ##
-function copy_main_resources {
+function copy_main_resources_pkgs {
         local MODULE_NAME=$1
 	local MAIN_RESOURCES_SOURCES=("${!2}")
-        local MODULE_DIR="${DEST_DIR}/${1}"
 
 	local MAIN_RESOURCES_FIND_ARGS=('-not' '-name' '*.java')
-	copy_mvn "${MODULE_NAME}" "${SRC_DIR}/src" MAIN_RESOURCES_SOURCES[@] 'src/main/resources' MAIN_RESOURCES_FIND_ARGS[@]
+	copy_mvn_pkgs "${MODULE_NAME}" "${SRC_DIR}/src" MAIN_RESOURCES_SOURCES[@] 'src/main/resources' MAIN_RESOURCES_FIND_ARGS[@]
 }
 
 ##
@@ -72,13 +71,12 @@ function copy_main_resources {
 # @param $1 The module name
 # @param $2 An array of package names which contain the sources
 ##
-function copy_test_java {
+function copy_test_java_pkgs {
         local MODULE_NAME=$1
         local TEST_JAVA_SOURCES=("${!2}")
-        local MODULE_DIR="${DEST_DIR}/${1}"
 
         local TEST_JAVA_FIND_ARGS=('-name' '*.java')
-        copy_mvn "${MODULE_NAME}" "${SRC_DIR}/test/src" TEST_JAVA_SOURCES[@] 'src/test/java' TEST_JAVA_FIND_ARGS[@]
+        copy_mvn_pkgs "${MODULE_NAME}" "${SRC_DIR}/test/src" TEST_JAVA_SOURCES[@] 'src/test/java' TEST_JAVA_FIND_ARGS[@]
 }
 
 ##
@@ -87,13 +85,12 @@ function copy_test_java {
 # @param $1 The module name
 # @param $2 An array of package names which contain the resources
 ##
-function copy_test_resources {
+function copy_test_resources_pkgs {
         local MODULE_NAME=$1
         local TEST_RESOURCES_SOURCES=("${!2}")
-        local MODULE_DIR="${DEST_DIR}/${1}"
 
         local TEST_RESOURCES_FIND_ARGS=('-not' '-name' '*.java')
-        copy_mvn "${MODULE_NAME}" "${SRC_DIR}/test/src" TEST_RESOURCES_SOURCES[@] 'src/main/resources' TEST_RESOURCES_FIND_ARGS[@]
+        copy_mvn_pkgs "${MODULE_NAME}" "${SRC_DIR}/test/src" TEST_RESOURCES_SOURCES[@] 'src/main/resources' TEST_RESOURCES_FIND_ARGS[@]
 }
 
 ##
@@ -105,13 +102,13 @@ function copy_test_resources {
 # @param $4 The maven module directory, e.g. 'src/main/java'
 # @param $5 An array of arguments to the find command to locate specific file types.
 ##
-function copy_mvn {
+function copy_mvn_pkgs {
         local MODULE_NAME=$1
 	local SOURCE_BASE=$2
 	local SOURCES=("${!3}")
 	local MVN_SRC_DIR=$4
 	local FIND_ARGS=("${!5}")
-        local MODULE_DIR="${DEST_DIR}/${1}"
+        local MODULE_DIR="${DEST_DIR}/${MODULE_NAME}"
 
 	echo "Copying module files: ${MODULE_NAME}/${MVN_SRC_DIR}..."
 
@@ -134,6 +131,81 @@ function copy_mvn {
                 	done
 		fi
         done
+}
+
+##
+# Copies the files for src/main/java
+#
+# @param $1 The module name
+# @param $2 An array of absolute paths to source files
+##
+function copy_main_java_files {
+	local MODULE_NAME=$1
+	local MAIN_JAVA_SOURCES=("${!2}")
+
+	copy_mvn_files "${MODULE_NAME}" MAIN_JAVA_SOURCES[@] 'src/main/java'
+}
+
+##
+# Copies the files for src/main/resources
+#
+# @param $1 The module name
+# @param $2 An array of absolute paths to source files
+##
+function copy_main_resources_files {
+        local MODULE_NAME=$1
+        local MAIN_RESOURCES_SOURCES=("${!2}")
+
+        copy_mvn_files "${MODULE_NAME}" MAIN_RESOURCES_SOURCES[@] 'src/main/resources'
+}
+
+##
+# Copies the files for src/test/java
+#
+# @param $1 The module name
+# @param $2 An array of absolute paths to source files
+##
+function copy_test_java_files {
+        local MODULE_NAME=$1
+        local TEST_JAVA_SOURCES=("${!2}")
+
+        copy_mvn_files "${MODULE_NAME}" TEST_JAVA_SOURCES[@] 'src/test/java'
+}
+
+##
+# Copies the files for src/test/resources
+#
+# @param $1 The module name
+# @param $2 An array of absolute paths to source files
+##
+function copy_test_resources_files {
+        local MODULE_NAME=$1
+        local TEST_RESOURCES_SOURCES=("${!2}")
+
+        copy_mvn_files "${MODULE_NAME}" TEST_RESOURCES_SOURCES[@] 'src/test/resources'
+}
+
+
+function copy_mvn_files {
+	local MODULE_NAME=$1
+        local SOURCES=("${!2}")
+        local MVN_SRC_DIR=$3
+        local MODULE_DIR="${DEST_DIR}/${MODULE_NAME}"
+
+	echo "Copying module files: ${MODULE_NAME}/${MVN_SRC_DIR}..."
+
+	for src in "${SOURCES[@]}"
+	do
+		local REL_SOURCE_FILE=$(echo "$src" | sed -E "s|.*/src/(.*)|\1|g")
+		local DEST_FILE="${MODULE_DIR}/${MVN_SRC_DIR}/${REL_SOURCE_FILE}"
+		local DEST_DIR=$(dirname "${DEST_FILE}")
+
+		if [ ! -d "${DEST_DIR}" ]
+		then
+			mkdir -p "${DEST_DIR}"
+		fi
+		cp -v "${src}" "${DEST_FILE}"
+	done
 }
 
 ##
@@ -164,22 +236,46 @@ EOL
 }
 
 ##
-# Mavenizes an eXist-db Module
+# Mavenizes an eXist-db Module from package names
 #
 # @param $1 The module name
 ##
-function mavenize_module {
+function mavenize_module_pkgs {
 	local MODULE_NAME=$1
 	local MODULE_PACKAGES=("${!2}")
 
 	echo "Mavenizing module: ${MODULE_NAME}..."
 
-	#create_std_project_layout $MODULE_NAME
 	create_module $MODULE_NAME
-	copy_main_java $MODULE_NAME MODULE_PACKAGES[@]
-	copy_main_resources $MODULE_NAME MODULE_PACKAGES[@]
-	copy_test_java $MODULE_NAME MODULE_PACKAGES[@]
-	copy_test_resources $MODULE_NAME MODULE_PACKAGES[@]
+	copy_main_java_pkgs $MODULE_NAME MODULE_PACKAGES[@]
+	copy_main_resources_pkgs $MODULE_NAME MODULE_PACKAGES[@]
+	copy_test_java_pkgs $MODULE_NAME MODULE_PACKAGES[@]
+	copy_test_resources_pkgs $MODULE_NAME MODULE_PACKAGES[@]
+}
+
+###
+# Mavenizes an eXist-db Module from file names
+#
+# @param $1 The module name
+# @param $2 An array of absolute paths from the source to be used as the src/main/java files.
+# @param $3 An array of absolute paths from the source to be used as the src/main/resources files.
+# @param $4 An array of absolute paths from the source to be used as the src/test/java files.
+# @param $5 An array of absolute paths from the source to be used as the src/test/resources files.
+##
+function mavenize_module_files {
+	local MODULE_NAME=$1
+	local MODULE_MAIN_JAVA_FILES=("${!2}")
+	local MODULE_MAIN_RESOURCES_FILES=("${!3}")
+	local MODULE_TEST_JAVA_FILES=("${!4}")
+	local MODULE_TEST_RESOURCES_FILES=("${!5}")
+
+	echo "Mavenizing module: ${MODULE_NAME}..."
+
+	create_module $MODULE_NAME
+	copy_main_java_files $MODULE_NAME MODULE_MAIN_JAVA_FILES[@]
+	copy_main_resources_files $MODULE_NAME MODULE_MAIN_RESOURCES_FILES[@]
+        copy_test_java_files $MODULE_NAME MODULE_TEST_JAVA_FILES[@]
+        copy_test_resources_files $MODULE_NAME MODULE_TEST_RESOURCES_FILES[@]
 }
 
 ##
@@ -189,7 +285,7 @@ function mavenize_module {
 #
 # @out $PKGS will hold an array of unique package names
 ##
-function extract_package_names {
+function extract_jar_package_names {
 	local JAR_FILENAME=$1
 
 	echo -e ""
@@ -214,6 +310,97 @@ function extract_package_names {
 	PKGS=($(echo "${PKGS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 }
 
+##
+# Extracts file names from a Jar file
+#
+# @param $1 the name of the jar file in $SRC_DIR
+#
+# @out $FILES will hold an array of relative file paths
+##
+function extract_jar_file_names {
+        local JAR_FILENAME=$1
+
+        echo -e ""
+        echo "Extracting file names from ${JAR_FILENAME}..."
+
+        IFS=$'\r\n' GLOBIGNORE='*' command eval  'JAR_LIST=($(jar tvf $SRC_DIR/$JAR_FILENAME))'
+
+        FILES=()
+        for jarentry in "${JAR_LIST[@]}"
+        do  
+                local JAR_ENTRY_FILE=`echo ${jarentry} | cut -d' ' -f8`
+                local len=${#JAR_ENTRY_FILE}
+
+                # only files, not folders
+                if [ "${JAR_ENTRY_FILE:$len-1:1}" != "/" ]
+                then
+                        FILES+=("$JAR_ENTRY_FILE")
+                fi  
+        done
+}
+
+##
+# Filters an array of strings
+#
+# @param $1 An array of strings
+# @param $2 A regular expression to match
+#
+# @out $FILTERED_ARRAY will contain the filtered array
+##
+function filter_array {
+        local ARY=("${!1}")
+        local PATTERN=$2
+
+        FILTERED_ARRAY=()
+        for elem in "${ARY[@]}"
+        do
+                if [[ "${elem}" =~ $PATTERN ]]
+                then
+                        FILTERED_ARRAY+=("$elem")
+                fi
+        done
+}
+
+##
+# Filters an array of strings to remove subclasses
+#
+# @param $1 An array of strings
+#
+# @out $FILTERED_ARRAY will contain the filtered array
+##
+function filter_notsubclass_array {
+        local ARY=("${!1}")
+
+        FILTERED_ARRAY=()
+        for elem in "${ARY[@]}"
+        do  
+                if [[ ! "${elem}" == *[\$]* ]]
+                then
+                        FILTERED_ARRAY+=("$elem")
+                fi  
+        done
+}
+
+##
+# Performs a replace on each string in an array of strings
+#
+# @param $1 An array of strings
+# @param $2 A Sed extended regular expression
+#
+# @out $REPLACES_ARRAY will contain the new array with replacements
+##
+function replace_array {
+        local ARY=("${!1}")
+        local EXPR=$2
+
+        REPLACED_ARRAY=()
+        for elem in "${ARY[@]}"
+        do
+		local NEW_ELEM=$(echo "${elem}" | sed -E "${EXPR}")
+                REPLACED_ARRAY+=("$NEW_ELEM")
+        done
+}
+
 
 echo -e ""
 echo "Reading eXist-db from ${SRC_DIR}..."
@@ -229,18 +416,47 @@ cp -v exist-parent.pom "${DEST_DIR}/exist-parent/pom.xml"
 
 ###  Mavenize each eXist-db Module
 
-#extract_package_names 'start.jar'
+#extract_jar_package_names 'start.jar'
 #EXIST_START_PKGS=( "${PKGS[@]}" )
-#mavenize_module exist-start EXIST_START_PKGS[@]
+#mavenize_module_pkgs exist-start EXIST_START_PKGS[@]
 
 ## Note there is a cicular dependency between the source files in start.jar, exist.jar and exist-optional.jar, so for now we just put both into exist-core
-extract_package_names 'start.jar'
+extract_jar_package_names 'start.jar'
 EXIST_CORE_PKGS=("${PKGS[@]}")
-extract_package_names 'exist.jar'
+extract_jar_package_names 'exist.jar'
 EXIST_CORE_PKGS+=("${PKGS[@]}")
-extract_package_names 'exist-optional.jar'
+extract_jar_package_names 'exist-optional.jar'
 EXIST_CORE_PKGS+=("${PKGS[@]}")
-mavenize_module exist-core EXIST_CORE_PKGS[@]
+mavenize_module_pkgs 'exist-core' EXIST_CORE_PKGS[@]
+# additional files needed
+ADDITIONAL_TEST_JAVA_PKGS=(
+	'org/exist/xmldb/concurrent'
+	'org/exist/xmldb/concurrent/action'
+	'org/exist/util/sorters'
+)
+copy_test_java_pkgs 'exist-core' ADDITIONAL_TEST_JAVA_PKGS[@]
+cp -v "${OUR_DIR}/exist-core.test.conf.xml" "${DEST_DIR}/exist-core/src/test/resources/conf.xml"
+# Antlr2 parsers should be generated as part of the build
+rm "${DEST_DIR}/exist-core/src/test/java/org/exist/xquery/functions/util/CounterTest.java"		# TODO(AR)temp, we need to move this to the counter module
+mkdir -p "${DEST_DIR}/exist-core/src/main/antlr/org/exist/xquery/parser"
+mv -v ${DEST_DIR}/exist-core/src/main/resources/org/exist/xquery/parser/*.g "${DEST_DIR}/exist-core/src/main/antlr/org/exist/xquery/parser"
+find "${DEST_DIR}/exist-core/src/main/java/org/exist/xquery/parser" -type f -maxdepth 1 -not -name \*AST.java -exec rm {} \;
+rm -rf "${DEST_DIR}/exist-core/src/main/resources/org/exist/xquery/parser"
+mkdir -p "${DEST_DIR}/exist-core/src/main/antlr/org/exist/xquery/xqdoc/parser"
+mv -v ${DEST_DIR}/exist-core/src/main/resources/org/exist/xquery/xqdoc/parser/*.g "${DEST_DIR}/exist-core/src/main/antlr/org/exist/xquery/xqdoc/parser"
+rm -rf "${DEST_DIR}/exist-core/src/main/java/org/exist/xquery/xqdoc/parser" "${DEST_DIR}/exist-core/src/main/resources/org/exist/xquery/xqdoc/parser"
+
+extract_jar_file_names 'exist-testkit.jar'
+EXIST_TESTKIT_FILES=("${FILES[@]}")
+filter_array EXIST_TESTKIT_FILES[@] '\.class$'
+EXIST_TESTKIT_FILES=("${FILTERED_ARRAY[@]}")
+filter_notsubclass_array EXIST_TESTKIT_FILES[@]
+EXIST_TESTKIT_FILES=("${FILTERED_ARRAY[@]}")
+BASE_SRC_PATH="${SRC_DIR}/test/src/"
+replace_array EXIST_TESTKIT_FILES[@] "s|(.*)\.class\$|${BASE_SRC_PATH}\1.java|g"
+EXIST_TESTKIT_FILES=("${REPLACED_ARRAY[@]}")
+# mavenize_module_files 'exist-testkit' EXIST_TESTKIT_FILES[@]  ## Note there is a circular dependency between the source files in exist-testkit.jar and the tests for exist.jar, so for now we also put these into exist-core
+copy_test_java_files 'exist-core' EXIST_TESTKIT_FILES[@]
 
 create_mvn_java_git_ignore 
 
